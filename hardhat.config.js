f1e6f5698bf649c1a5435b09caec00b9772c6454
tom hikari <tomprescott1565@gmail.com>
2025-05-24 07:07:42 +0100

    Update Hardhat configuration to use Solidity 0.8.19 and add Sepolia network settings. Remove Lock contract and related files, and update package scripts for deployment and testing.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.28",
+// };
+
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+ 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
 };
